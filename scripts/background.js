1b353f0c141e48f8f151b97eb93d3c14bc1acab2
dsilvaro <DANIEL-ALEJANDRO.SILVA-ROMERO@external.t-systems.com>
2023-11-15 14:01:39 +0100

    check holidays setup

--- scripts/background.js
+++ scripts/background.js
@@ -3,18 +3,30 @@ let result = {};
 let i_von = document.querySelector('[name=i_von]');
 let i_bis = document.querySelector('[name=i_bis]');
 let autoFillBtnContainer = document.querySelector("#delayedDisplay > form > table > tbody > tr:nth-child(1) > td:nth-child(2)");
-let autoFillButton = document.createElement('input')
 let weekFields = [...document.querySelectorAll("#delayedDisplay > form > table > tbody input[type=text]")];
+let holidays = [];
+let weekDays = [...document.querySelectorAll("#delayedDisplay > form > table > tbody > tr:nth-child(1) input[name=i_datum]")].map(e => {
+  let arr = e.value.split('.');
+
+  return [
+    Math.floor(arr[0]),
+    Math.floor(arr[1]),
+    Math.floor(arr[2]),
+  ];
+});
+
 
 async function loadData() {
   
   let result = await chrome.storage.sync.get(["holidaysPerYear"]);
+  holidays = result.holidaysPerYear.holidays;
+  addAutoFillButton();
 
-
-  console.log('data? ', result, i_von, i_bis);
+  console.log('data? ', holidays, weekDays);
 };
 
 function addAutoFillButton() {
+  let autoFillButton = document.createElement('input')
   autoFillButton.id = "autofillBtn";
   autoFillButton.type = "button";
   autoFillButton.value = "Autofill Week";
@@ -28,32 +40,36 @@ function addAutoFillButton() {
 function fillWeek() {
   let weekLength = 0;
   let rowLength= 0;
+  let current = 0;
 
   weekFields.every(e => {
     let notWeekend = e.className !== 'weekend';
 
-    if(checkHoliday()) return false;
+    if(!notWeekend || e.name === 'i_row_sum') {
+      current = 0;
+      return true;
+    };
 
     if(e.name === 'i_kommt' && notWeekend) {
-        e.value = '08:00';
-        weekLength++;
+      weekLength++;
+      e.value = checkHoliday(current) ? '' : '08:00';
     } else if(e.name === 'i_geht' && notWeekend) {
-        e.value = '16:00';
+        e.value = checkHoliday(current) ? '' : '16:00';
     } else if(e.name === 'i_az' ) {
       if (!notWeekend || rowLength >= weekLength) return false;
         rowLength++;
-        e.value = '08:00';
+        e.value = checkHoliday(current) ? '' : '08:00';
     }
 
+    current++;
     return true;
   });
 
   console.log(weekFields)
 }
 
-function checkHoliday() {
-
+function checkHoliday(current) {
+  return holidays.some(e => e.join('.') === weekDays[current].join('.'))
 }
 
 loadData();
-addAutoFillButton();
