3268a9d4efa88136ab5b89dd15a0b59fdae80c2f
dsilvaro <DANIEL-ALEJANDRO.SILVA-ROMERO@external.t-systems.com>
2023-11-15 22:42:43 +0100

    fixed icons and manifest

---
+++ default.js
@@ -0,0 +1,21 @@
+defaultData = {
+  "endTime": "16:00",
+  "holidays": [
+    [6, 1, 2023],
+    [7, 4, 2023],
+    [1, 5, 2023],
+    [15, 8, 2023],
+    [12, 10, 2023],
+    [1, 11, 2023],
+    [6, 12, 2023],
+    [8, 12, 2023],
+    [25, 12, 2023]
+  ],
+  "holidaysType": "web",
+  "location": "todas",
+  "raw": "6 de enero: Reyes\n7 de abril: Viernes Santo.\n1 de Mayo: Día del Trabajo\n15 de agosto: Asunción de la Virgen.\n12 de octubre: Día de la Hispanidad\n1 de noviembre: Todos los Santos.\n6 de diciembre: Día de la Constitución Española.\n8 de diciembre: Inmaculada Concepción.\n25 de diciembre: Navidad.",
+  "separator": "",
+  "startTime": "08:00",
+  "totalTime": "08:00",
+  "year": 2023
+}
